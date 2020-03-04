8fa57209a3a1bbb6b3ce3f70a772faed8eeb167f
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 20:52:44 -0300

    comit branch

--- src/br/fai/projeto04/Main.java
+++ src/br/fai/projeto04/Main.java
@@ -3,8 +3,13 @@ package br.fai.projeto04;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Main app = new Main();
+		app.start();
+	}
 
+	private void start() {
+		
+		System.out.println("enviando branch para o github");
 	}
 
 }
