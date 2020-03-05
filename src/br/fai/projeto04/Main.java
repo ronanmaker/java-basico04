ecf0e78aaa4b2eea096112889b2f5f7938bafbb3
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 21:01:54 -0300

    resolução

--- src/br/fai/projeto04/Main.java
+++ src/br/fai/projeto04/Main.java
@@ -10,6 +10,20 @@ public class Main {
 	private void start() {
 		
 		System.out.println("enviando branch para o github");
+		for (int i = 0; i <5; i ++) {
+			switch (i) {
+			case 1:
+				System.out.println("Primeira condição");
+				break;
+				
+			case 4:
+				System.out.println("Segunda Condição");
+			
+			default:
+			System.out.println("Não encontrou nenhuma condição");
+			break;
+			}
+		}
 	}
 
 }
